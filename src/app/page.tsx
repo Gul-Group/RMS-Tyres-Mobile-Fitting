95359f801e58b540193b56af25a1626e47ca5101
Gulfam-Sidhu <Clavercracker.gul@gmail.com>
2025-08-16 00:52:38 +0500

    Clean Up all Code and Ready to Start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { TvIcon } from "@heroicons/react/16/solid";
-import { Button } from "@heroui/react";
-
-export default function Home() {  
-  return (
-    <main>
-      <p>Here is Text</p>
-      <TvIcon className="size-5 text-blue-500"/>
-      <Button color="primary">Button</Button>
-    </main>
-  );
+export default function Home() {
+  return <main></main>;
 }
