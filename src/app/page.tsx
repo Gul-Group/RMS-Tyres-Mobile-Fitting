57c8dcf17f79496e8b84d6739e4afd9fda03004b
Gulfam-Sidhu <Clavercracker.gul@gmail.com>
2025-08-16 00:46:38 +0500

    Setup Hero UI in Next UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { TvIcon } from "@heroicons/react/16/solid";
+import { Button } from "@heroui/react";
+
+export default function Home() {  
   return (
     <main>
       <p>Here is Text</p>
+      <TvIcon className="size-5 text-blue-500"/>
+      <Button color="primary">Button</Button>
     </main>
   );
 }
