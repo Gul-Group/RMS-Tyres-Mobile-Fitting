362afc442357258822d2a8ea9e8a0a781c472165
Gulfam Sidhu <Clavercracker.gul@gmail.com>
2025-10-11 06:28:35 -0700

    Radix Testing and CSS implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 export const metadata: Metadata = {
   title: "RMS Tyres Mobile Fitting",
@@ -14,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Theme>
           {children}
+        </Theme>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <main>
+      <Flex direction="row" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
     </main>
   );
 }
